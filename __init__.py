96708ba963c2b699a0c30a3818852ca5f7dce9ea
Glenn <gjgalvizo@gmail.com>
2020-11-27 11:47:15 -0800

    Creating the log directory if it doesn't exist.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,11 @@
 import logging.config
 import json
+import os
 
 with open('config/logging.json') as logging_config_file:
     logging_json = json.load(logging_config_file)
+    logging_output_dir = logging_json['handlers']['fileHandler']['filename'].split('/')[0]
+    if not os.path.exists(logging_output_dir):
+        os.makedirs(logging_output_dir)
+
     logging.config.dictConfig(logging_json)
