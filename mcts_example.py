5b6547551e454298d116a62da7a56edde8e2aec0
Glenn <gjgalvizo@gmail.com>
2020-12-15 00:06:28 -0800

    Adding some markers for the log file.

--- mcts_example.py
+++ mcts_example.py
@@ -1,17 +1,25 @@
 import single_player_mcts as mc
 import game_model as gm
+import logging
+
+logger = logging.getLogger(__name__)
+
 
 if __name__ == '__main__':
-    root_state = gm.State.build('resources/from_class/sokoban01.txt')
-    root_node = mc.Node(root_state)
-    x = mc.MCTS(**{
-        'root': root_node,
+    input_file = 'resources/from_class/benchmark/sokoban02.txt'
+    mcts_params = {
         'heuristic_f': gm.Evaluation.heuristic_3,
-        'simulation_bound': 250,
+        'simulation_bound': 300,
         'exploration_c': 0.5,
         'uncertainty_d': 50.0,
         'heuristic_correction_f': gm.Evaluation.heuristic_3_correction
-    })
+    }
+    logger.info(f'Starting new run for input file: {input_file}.')
+    logger.info(f'MCTS parameters: {mcts_params}')
+
+    root_state = gm.State.build(input_file)
+    root_node = mc.Node(root_state)
+    x = mc.MCTS(**{**mcts_params, **{'root': root_node}})
 
     # Visualize our run.
     gm.Visual.start_instance()
@@ -19,4 +27,4 @@ if __name__ == '__main__':
 
     # Print our solution.
     gm.Visual.kill_instance()
-    print(solution)
+    logger.info(f'Solution: {solution}')
